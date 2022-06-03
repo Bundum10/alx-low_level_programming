12f58c0b143e47211e42e69ba32e0b51fe0df929
Iwegbu Chibundum <ichibundum@gmail.com>
2022-06-03 00:28:25 -0700

    Final task

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,13 +1,12 @@
 #include <stdio.h>
+
 /**
- * main - main block
- *
- * Description: with proper grammar, but the outcome is a piece of art.
- *
- * Returns:  Always 0 (Success)
- */
+* main - Entry block
+* 
+* Return: Always 0 (Success)
+*/
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,\n");
+	printf("\"with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
