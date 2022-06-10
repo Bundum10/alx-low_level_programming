dbe808a696e4d4e1d249aa2e97be850a08248180
Iwegbu Chibundum <ichibundum@gmail.com>
2022-06-09 19:32:30 -0700

    Update

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -13,20 +13,13 @@ int main(void)
 
 	i = 0;
 
-/**
+/*
 *	while (i < 10)
-*
 *       {
-*
 *       Infinite loop - No increase of variable
-*
 *       putchar(i);
-*
 *       }
-*
 */
-
-	printf("Infinite loop avoided! \\o/\n");
-
+	printf("Infinite loop avoided! \\o/\n";
 	return (0);
 }
