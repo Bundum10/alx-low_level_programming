80cdb7b2f408a858b10511aa71a31d70a11076bb
Iwegbu Chibundum <ichibundum@gmail.com>
2022-06-24 21:11:25 -0700

    argc & argc

---
+++ 0x0A-argc_argv/main.h
@@ -0,0 +1,7 @@
+#ifndef _MAIN_H_
+#define _MAIN_H_
+
+int main(int argc __attribute__((unused)), char *argv[]);
+int main(int argc, char *argv[] __attribute__((unused)));
+int main(int argc, char *argv[]);
+#endif
